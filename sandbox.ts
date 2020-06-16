6f42de427d5dcc4e8c0ebdff64b446b9b82b7bca
rodmartinezmedina <rodmartinezmedina@gmail.com>
2020-06-16 14:21:26 +0200

    .

--- sandbox.ts
+++ sandbox.ts
@@ -1,17 +1,23 @@
-var age = 25;
+let age: any = 25;
+
 age = true;
 console.log(age);
 age = "hello";
 console.log(age);
 age = { name: "luigi" };
 console.log(age);
-var mixed = [];
+
+let mixed: any[] = [];
+
 mixed.push(5);
 mixed.push("mario");
 mixed.push(false);
 console.log(mixed);
-var ninja;
+
+let ninja: { name: any; age: any };
+
 ninja = { name: "yoshi", age: 25 };
 console.log(ninja);
+
 ninja = { name: 25, age: "yoshi" };
 console.log(ninja);
